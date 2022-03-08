84fd781bc479aaf3b313d2a889330607b53e055e
Airu <Rizkykirigaya@gmail.com>
2022-03-08 09:55:07 +0700

    fix minor bug

--- pages/pagination/index.js
+++ pages/pagination/index.js
@@ -69,12 +69,8 @@ function Pagination({ digi }) {
             }}
             className="h-12 px-4 bg-white border-2 border-r-0 border-indigo-600 rounded-l-lg hover:bg-indigo-600 hover:text-white"
           >
-            <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
-              <path
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-              />
+            <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+              <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" />
             </svg>
           </button>
           {pageNumbers.map((item, i) => (
@@ -95,12 +91,8 @@ function Pagination({ digi }) {
             }}
             className="h-12 px-4 bg-white border-2 border-indigo-600 rounded-r-lg hover:bg-indigo-600 hover:text-white"
           >
-            <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
-              <path
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-              />
+            <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+              <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" />
             </svg>
           </button>
         </div>
