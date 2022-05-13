3260dc41afee448babbadcb8d95470a26426de08
Mario Alexander <mario.alexander@tiket.com>
2022-05-13 15:20:47 +0700

    useI18n

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,18 @@ import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Layout from "../components/Layout";
 
+// Lib
+import { useI18n } from './../lib/i18n/index';
+
 export default function Home() {
+  const i18n = useI18n();
+
+  const wording = i18n(`common.i`);
+
   return (
     <Layout pageTitle="Home Page">
       <p>Ini Halaman Home</p>
+      <p>Wording: {wording}</p>
     </Layout>
   );
 }
